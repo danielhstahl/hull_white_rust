19ae51a64cb1955b2ac25370df2708fe4c346b8f
Daniel Stahl <danstahl1138@gmail.com>
2019-08-26 17:36:26 +0000

    added tests and updated travis

--- benches/bench.rs
+++ benches/bench.rs
@@ -86,6 +86,106 @@ fn bench_coupon_bond(c: &mut Criterion){
     }));
 }
 
+fn bench_swap_rate(c: &mut Criterion){
+    let curr_rate = 0.05;
+    let sig: f64 = 0.01;
+    let a: f64 = 0.05;
+    let b = 0.05;
+    let delta = 0.25;
+    let future_time = 0.0;
+    let swap_tenor = 5.0;
+    let option_maturity = 1.0;
+    let yield_curve = move |t: f64| {
+        let at = (1.0 - (-a * t).exp()) / a;
+        let ct =
+            (b - sig.powi(2) / (2.0 * a.powi(2))) * (at - t) - (sig * at).powi(2) / (4.0 * a);
+        at * curr_rate - ct
+    };
+    let forward_curve = move |t: f64| {
+        b + (-a * t).exp() * (curr_rate - b)
+            - (sig.powi(2) / (2.0 * a.powi(2))) * (1.0 - (-a * t).exp()).powi(2)
+    };
+    c.bench_function("swap_rate", move |b|{
+        b.iter(||{
+            hull_white::forward_swap_rate_t(
+                curr_rate,
+                a,
+                sig,
+                future_time,
+                option_maturity,
+                swap_tenor + option_maturity,
+                delta,
+                &yield_curve,
+                &forward_curve,
+            )
+        })
+    });
+}
 
-criterion_group!(benches, bench_bond, bench_coupon_bond);
+fn bench_swaption_bond(c: &mut Criterion){
+    let curr_rate = 0.05;
+    let sig: f64 = 0.01;
+    let a: f64 = 0.05;
+    let b = 0.05;
+    let delta = 0.25;
+    let future_time = 0.0;
+    let swap_tenor = 5.0;
+    let option_maturity = 1.0;
+    let yield_curve = move |t: f64| {
+        let at = (1.0 - (-a * t).exp()) / a;
+        let ct =
+            (b - sig.powi(2) / (2.0 * a.powi(2))) * (at - t) - (sig * at).powi(2) / (4.0 * a);
+        at * curr_rate - ct
+    };
+    let forward_curve = move |t: f64| {
+        b + (-a * t).exp() * (curr_rate - b)
+            - (sig.powi(2) / (2.0 * a.powi(2))) * (1.0 - (-a * t).exp()).powi(2)
+    };
+    let swap_rate = hull_white::forward_swap_rate_t(
+        curr_rate,
+        a,
+        sig,
+        future_time,
+        option_maturity,
+        swap_tenor + option_maturity,
+        delta,
+        &yield_curve,
+        &forward_curve,
+    );
+    c.bench("compare swaption tree", ParameterizedBenchmark::new("analytical",move |b, _i|{
+        b.iter(||{
+            hull_white::european_payer_swaption_t(
+                curr_rate,
+                a,
+                sig,
+                future_time,
+                swap_tenor,
+                option_maturity,
+                delta,
+                swap_rate,
+                &yield_curve,
+                &forward_curve,
+            )
+        })
+    }, vec![100, 500, 1000]).with_function("tree", move |b, i|{
+        b.iter(||{
+            hull_white::american_payer_swaption_t(
+                curr_rate,
+                a,
+                sig,
+                future_time,
+                swap_tenor,
+                option_maturity,
+                delta,
+                swap_rate,
+                *i,
+                &yield_curve,
+                &forward_curve,
+            )
+        })
+    }));
+}
+
+
+criterion_group!(benches, bench_bond, bench_coupon_bond, bench_swaption_bond, bench_swap_rate);
 criterion_main!(benches);
